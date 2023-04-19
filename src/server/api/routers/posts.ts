1e60db7158a5c43a04d1548a75fc1042fa1d1d5c
djhxiaoming <nickazurewilson@gmail.com>
2023-04-18 23:33:14 -0700

    rate limites

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,6 +5,20 @@ import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(5, "60 s"),
+    analytics: true,
+    /**
+     * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+     * instance with other applications and want to avoid key collisions. The default prefix is
+     * "@upstash/ratelimit"
+     */ 
+    prefix: "@upstash/ratelimit",
+  });
 
 const filterUserForClient = (user: User) => {
     const name = user.firstName;
@@ -39,6 +53,8 @@ export const postsRouter = createTRPCRouter({
     content:z.string().min(1),
   })).mutation(async ({ctx, input}) => {
     const authorId = ctx.curretnUserId;
+    const {success} = await ratelimit.limit(authorId);
+    if (!success) throw new TRPCError({code: "BAD_REQUEST", message: "发送请求太过频繁"});
 
     const post = await ctx.prisma.post.create({
         data:{
